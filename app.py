a6b477e92c927185b3c57d65072121ddf674f7c8
Chris Simmerman <chrisjsimmerman@gmail.com>
2022-10-24 21:39:56 -0400

    added graph, range slider, and 3rd callback

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ rating_bo2 = rating_bo.groupby(['rating', 'year'])['gross'].sum().reset_index(na
 
 fig = px.line(genre_bo2, x="year", y="gross", color='genre', markers=True)
 fig2 = px.line(rating_bo2, x="year", y="gross", color='rating', markers=True)
+fig3 = px.histogram(df, x='year', y='gross', color='genre', barmode='group')
 
 app.layout = html.Div(children=[
     html.Div([
@@ -41,6 +42,26 @@ app.layout = html.Div(children=[
             figure=fig2
         ), 
     ]),
+    html.Div([
+        html.H1(children='Histogram of Gross by Genre per Year'),
+
+        html.Div(),
+        dcc.RangeSlider(
+            min=1980,
+            max=2020,
+            step=None,
+            marks={1980: '1980', 1981: '1981', 1982: '1982', 1983: '1983', 1984: '1984', 1985: '1985', 1986: '1986', 1987: '1987', 1988: '1988', 1989: '1989',
+                   1990: '1990', 1991: '1991', 1992: '1992', 1993: '1993', 1994: '1994', 1995: '1995', 1996: '1996', 1997: '1997', 1998: '1998', 1999: '1999',
+                   2000: '2000', 2001: '2001', 2002: '2002', 2003: '2003', 2004: '2004', 2005: '2005', 2006: '2006', 2007: '2007', 2008: '2008', 2009: '2009',
+                   2010: '2010', 2011: '2011', 2012: '2012', 2013: '2013', 2014: '2014', 2015: '2015', 2016: '2016', 2017: '2017', 2018: '2018', 2019: '2019', 2020: '2020' 
+    },
+    value=[1980, 2020], id='my-range-slider'
+),
+        dcc.Graph(
+            id='display-selected-values-3',
+            figure=fig3
+        ),   
+    ])
 ])
 
 
@@ -59,6 +80,14 @@ def update_output2(value):
     ts = rating_bo2[rating_bo2["rating"].isin(value)]
     fig2 = px.line(ts, x="year", y="gross", color='rating', markers=True)
     return fig2
+
+@app.callback(
+    dash.dependencies.Output('display-selected-values-3', 'figure'),
+    [dash.dependencies.Input('my-range-slider', 'value')])
+def update_output3(value):
+    ts = df[df["year"].isin(value)]
+    fig3 = px.histogram(ts, x="year", y="gross", color='genre', barmode='group')
+    return fig3
 
 if __name__ == '__main__':
     app.run_server(debug=True)
