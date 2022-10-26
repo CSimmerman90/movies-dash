8f949b5401cb01152a0a7da9182e728b8281d084
Chris Simmerman <chrisjsimmerman@gmail.com>
2022-10-25 21:24:17 -0400

    commit with broken code

--- app.py
+++ app.py
@@ -45,18 +45,21 @@ app.layout = html.Div(children=[
     html.Div([
         html.H1(children='Histogram of Gross by Genre per Year'),
 
-        html.Div(),
-        dcc.RangeSlider(
-            min=1980,
-            max=2020,
-            step=None,
-            marks={1980: '1980', 1981: '1981', 1982: '1982', 1983: '1983', 1984: '1984', 1985: '1985', 1986: '1986', 1987: '1987', 1988: '1988', 1989: '1989',
-                   1990: '1990', 1991: '1991', 1992: '1992', 1993: '1993', 1994: '1994', 1995: '1995', 1996: '1996', 1997: '1997', 1998: '1998', 1999: '1999',
-                   2000: '2000', 2001: '2001', 2002: '2002', 2003: '2003', 2004: '2004', 2005: '2005', 2006: '2006', 2007: '2007', 2008: '2008', 2009: '2009',
-                   2010: '2010', 2011: '2011', 2012: '2012', 2013: '2013', 2014: '2014', 2015: '2015', 2016: '2016', 2017: '2017', 2018: '2018', 2019: '2019', 2020: '2020' 
-    },
-    value=[1980, 2020], id='my-range-slider'
-),
+        html.Div([
+            dcc.RangeSlider(
+                 min=1980,
+                 max=2020,
+                 step=None,
+                 marks={1980: '1980', 1981: '1981', 1982: '1982', 1983: '1983', 1984: '1984', 1985: '1985', 1986: '1986', 1987: '1987', 1988: '1988', 1989: '1989',
+                        1990: '1990', 1991: '1991', 1992: '1992', 1993: '1993', 1994: '1994', 1995: '1995', 1996: '1996', 1997: '1997', 1998: '1998', 1999: '1999',
+                        2000: '2000', 2001: '2001', 2002: '2002', 2003: '2003', 2004: '2004', 2005: '2005', 2006: '2006', 2007: '2007', 2008: '2008', 2009: '2009',
+                        2010: '2010', 2011: '2011', 2012: '2012', 2013: '2013', 2014: '2014', 2015: '2015', 2016: '2016', 2017: '2017', 2018: '2018', 2019: '2019', 2020: '2020' 
+                },
+                value=[1980, 1985], id='my-range-slider'
+            ),
+            dcc.Checklist(id= 'my-checklist', options=[{"value": i, "label": i} for i in df['genre'].unique()],
+                     value= ['Action']),
+        ]),
         dcc.Graph(
             id='display-selected-values-3',
             figure=fig3
@@ -84,10 +87,27 @@ def update_output2(value):
 @app.callback(
     dash.dependencies.Output('display-selected-values-3', 'figure'),
     [dash.dependencies.Input('my-range-slider', 'value')])
-def update_output3(value):
-    ts = df[df["year"].isin(value)]
-    fig3 = px.histogram(ts, x="year", y="gross", color='genre', barmode='group')
-    return fig3
+def set_cities_options(selected_country):
+    return [{'label': i, 'value': i} for i in all_options[selected_country]]
+
+
+@app.callback(
+    dash.dependencies.Output('cities-radio', 'value'),
+    [dash.dependencies.Input('cities-radio', 'options')])
+def set_cities_value(available_options):
+    return available_options[0]['value']
+
+
+@app.callback(
+    dash.dependencies.Output('display-selected-values', 'children'),
+    [dash.dependencies.Input('countries-radio', 'value')],
+    [dash.dependencies.Input('cities-radio', 'value')])
+def set_display_children(selected_country, selected_city):
+    return u'{} is a city in {}'.format(
+        selected_city, selected_country,
+    )
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
